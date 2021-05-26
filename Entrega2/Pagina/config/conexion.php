85494837a62d520685c81b1ca0f01bc3b49da8ca
martin alonso ponce carrasco <martin.poncec@uc.cl>
2021-05-25 20:55:56 -0400

    avance

--- Entrega2/Pagina/config/conexion.php
+++ Entrega2/Pagina/config/conexion.php
@@ -3,7 +3,7 @@
     #Pide las variables para conectarse a la base de datos.
     require('data.php'); 
     # Se crea la instancia de PDO
-    $db = new PDO("pgsql:dbname=$databaseName;host=localhost;port=5555;user=$user;password=$password");
+    $db = new PDO("pgsql:dbname=$databaseName;host=localhost;port=80;user=$user;password=$password");
   } catch (Exception $e) {
     echo "No se pudo conectar a la base de datos: $e";
   }
