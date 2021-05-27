5296e597727c2d9e49f6419f297b5968de1fa254
martin alonso ponce carrasco <martin.poncec@uc.cl>
2021-05-27 17:15:40 -0400

    Avance

--- Sites/consultas/consulta_tipo.php
+++ Sites/consultas/consulta_tipo.php
@@ -3,27 +3,22 @@
 <body>
 
   <?php
-  require("../config/conexion.php"); #Llama a conexión, crea el objeto PDO y obtiene la variable $db
+  require("../config/conexion.php");
 
   $var = $_POST["tipo"];
-  $query = "SELECT * FROM pokemones WHERE tipo='$var';";
+  $query = "SELECT DISTINCT tiendas.nombre FROM tiendas, productos, productos_en_tienda WHERE productos.tipo='$var' AND tiendas.id=productos_en_tienda.tienda_id AND productos.id=productos_en_tienda.producto_id;";
   $result = $db -> prepare($query);
   $result -> execute();
-  $dataCollected = $result -> fetchAll(); #Obtiene todos los resultados de la consulta en forma de un arreglo
+  $Tiendas = $result -> fetchAll(); 
   ?>
 
   <table>
     <tr>
-      <th>ID</th>
       <th>Nombre</th>
-      <th>Altura</th>
-      <th>Peso</th>
-      <th>Exp Base</th>
-      <th>Tipo</th>
     </tr>
   <?php
-  foreach ($dataCollected as $p) {
-    echo "<tr> <td>$p[0]</td> <td>$p[1]</td> <td>$p[2]</td> <td>$p[3]</td> <td>$p[4]</td> <td>$p[5]</td> </tr>";
+  foreach ($Tiendas as $Tienda) {
+    echo "<tr> <td>$Tienda[0]</td>  </tr>";
   }
   ?>
   </table>
--- Sites/index.php
+++ Sites/index.php
@@ -36,7 +36,8 @@
   <h3 align="center"> Usuarios que compraron un producto según su descripción</h3>
 
   <form align="center" action="consultas/consulta_descripcion.php" method="post">
-    Descripción:
+    Descripción
+    <br/><br/>
     <input type="text" name="descripcion">
     <br/><br/>
     <input type="submit" value="Buscar">
@@ -48,8 +49,9 @@
   <h3 align="center"> Edad promedio de trabajadores por comuna</h3>
 
   <form align="center" action="consultas/consulta_edad.php" method="post">
-    Descripción:
-    <input type="text" name="descripcion">
+    Comuna
+    <br/><br/>
+    <input type="text" name="comuna">
     <br/><br/>
     <input type="submit" value="Buscar">
   </form>
@@ -57,21 +59,19 @@
   <br>
   <br>
 
-  <h3 align="center">¿Quieres buscar todos los pokemones por tipo?</h3>
+  <h3 align="center">Tiendas que venden productos de ese tipo</h3>
 
   <?php
-  #Primero obtenemos todos los tipos de pokemones
   require("config/conexion.php");
-  $result = $db -> prepare("SELECT tiendas.nombre,despacho.comuna_de_cobertura FROM tiendas,despacho WHERE tiendas.id=despacho.tienda_id ORDER BY tiendas.id;");
+  $result = $db -> prepare("SELECT DISTINCT productos.tipo FROM productos ORDER BY productos.tipo;");
   $result -> execute();
   $dataCollected = $result -> fetchAll();
   ?>
 
   <form align="center" action="consultas/consulta_tipo.php" method="post">
-    Seleccinar un tipo:
+    Seleccione un tipo de producto:
     <select name="tipo">
       <?php
-      #Para cada tipo agregamos el tag <option value=value_of_param> visible_value </option>
       foreach ($dataCollected as $d) {
         echo "<option value=$d[0]>$d[0]</option>";
       }
