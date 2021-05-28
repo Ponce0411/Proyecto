46b07b0a0ec7f9bef10857e46b90649aa543a9ab
martin alonso ponce carrasco <martin.poncec@uc.cl>
2021-05-28 00:03:58 -0400

    Avance

--- Sites/consultas/consulta_mayor_venta.php
+++ Sites/consultas/consulta_mayor_venta.php
@@ -6,7 +6,7 @@
   require("../config/conexion.php");
 
   $var = $_POST["tipo"];
-  $query = "SELECT DISTINCT tiendas.nombre, SUM(productos_en_compra.cantidad) as cantidad FROM tiendas, productos, productos_en_compra, compras WHERE productos.tipo='$var' AND tiendas.id=compras.tienda_id AND compras.id=productos_en_compra.compra_id AND productos.id=productos_en_compra.producto_id GROUP BY tiendas.id ORDER BY cantidad DESC;";
+  $query = "SELECT DISTINCT tiendas.nombre, SUM(productos_en_compra.cantidad) as cantidad FROM tiendas, productos, productos_en_compra, compras WHERE productos.tipo='$var' AND tiendas.id=compras.tienda_id AND compras.id=productos_en_compra.compra_id AND productos.id=productos_en_compra.producto_id GROUP BY tiendas.id ORDER BY cantidad DESC LIMIT 10;";
   $result = $db -> prepare($query);
   $result -> execute();
   $Tiendas = $result -> fetchAll(); 
