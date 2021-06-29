d4c4605cfd7027695bf111c83ab0df766fe90bd0
martin alonso ponce carrasco <martin.poncec@uc.cl>
2021-06-28 21:00:22 -0400

    Avance

---
+++ Entrega3/insertar_usuarios.sql
@@ -0,0 +1,28 @@
+CREATE OR REPLACE FUNCTION
+insertar_usuarios()
+RETURNS void AS $$
+
+DECLARE
+tupla_persona2 RECORD ;
+maximo INT ;
+
+BEGIN
+    FOR tupla_persona2 IN (SELECT * FROM public.dblink('dbname=grupo48e3 
+    port=5432
+    password=tuckathomas123
+    user=grupo48','SELECT persoanl.id, personal.nombre,personal.rut,personal.sexo
+    FROM personal WHERE personal.clasificacion = 'administracion'')AS f(id int, nombre varchar, rut int, sexo varchar)
+    
+
+    LOOP
+
+    SET maximo = SELECT MAX(usuarios.id) FROM usuarios;
+
+    IF (tupla_persona2  NOT IN (SELECT * FROM usuarios) THEN
+    
+    INSERT INTO usuarios VALUES(maximo+1,tupla_persona2.nombre,tupla_persona2.rut,tupla_persona2.sexo);
+    END LOOP;
+
+END
+
+$$ language plpgsql
