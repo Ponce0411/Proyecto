3175fdb7e8f43e24fb91d94a2d7b47b07be202b6
martin alonso ponce carrasco <martin.poncec@uc.cl>
2021-07-05 06:20:38 -0400

    COOOORRSS

--- Entrega3/listado_compras.sql
+++ Entrega3/listado_compras.sql
@@ -14,7 +14,7 @@ BEGIN
     FOR tupla_compra IN (SELECT * FROM public.dblink('dbname=grupo48e3 
     port=5432
     password=tuckathomas123
-    user=grupo48','SELECT despachos.compra_id, despachos.fecha FROM despachos') AS f(id_compra int, fecha date))
+    user=grupo48','SELECT despachos.compra_id, despachos.fecha FROM despachos') AS f(id_compra int, fecha varchar))
 
     LOOP
 
