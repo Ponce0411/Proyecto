963cf06062c07e2346e8e8a88b6a87aa15a4fcf7
martin alonso ponce carrasco <martin.poncec@uc.cl>
2021-07-05 05:17:11 -0400

    Avance

--- Entrega3/listado_compras.sql
+++ Entrega3/listado_compras.sql
@@ -1,19 +1,29 @@
 CREATE OR REPLACE FUNCTION
-listado_compra()
+Enguels()
 RETURNS void AS $$
 
 DECLARE
 tupla_compra RECORD ;
 
 BEGIN
+
+    IF NOT EXISTS(SELECT * FROM information_schema.COLUMNS WHERE COLUMN_NAME = 'fecha' AND TABLE_NAME='compras') THEN
+    ALTER TABLE compras ADD COLUMN fecha date;
+    END IF; 
+
     FOR tupla_compra IN (SELECT * FROM public.dblink('dbname=grupo48e3 
     port=5432
     password=tuckathomas123
     user=grupo48','SELECT despachos.id_compra, despachos.fecha FROM despachos') AS f(id_compra int, fecha date))
 
-    IF NOT EXISTS(SELECT * FROM information_schema.COLUMNS WHERE COLUMN_NAME = 'fecha' AND TABLE_NAME='compras') THEN
-    ALTER TABLE compras ADD COLUMN fecha date;
-    END IF; 
+    LOOP
+
+    UPDATE compras
+    SET fecha = listado.enguels
+    FROM compras, (SELECT compras.id, tupla_compra.fecha AS enguels FROM compras, tupla_compra WHERE tupla_compra.id_compra = compras.id) 
+    AS listado WHERE tupla_compra.id_compra = compras.id
+
+    END LOOP;
 
 END
 
