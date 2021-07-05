cba0fb915996cd83c6e6f2cb23f7f0379d7baa66
Lucas <69730343+Lucassalcedo@users.noreply.github.com>
2021-07-05 07:57:38 -0400

    fgadfat

--- Entrega3/generar_contraseñas.sql
+++ Entrega3/generar_contraseñas.sql
@@ -4,7 +4,7 @@ RETURNS void AS $$
 
 BEGIN
     IF NOT EXISTS(SELECT * FROM information_schema.COLUMNS WHERE COLUMN_NAME = 'contraseña' AND TABLE_NAME='usuarios_web') THEN
-    ALTER TABLE usuarios_web ADD COLUMN contraseña varchar(4);
+    ALTER TABLE usuarios_web ADD COLUMN contraseña varchar(100);
     END IF; 
 
     UPDATE usuarios_web
