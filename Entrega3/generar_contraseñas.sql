89ea2d6bf0a9afebfb5690a5027d76e1f1a667a5
Lucas <69730343+Lucassalcedo@users.noreply.github.com>
2021-07-05 08:04:43 -0400

    dadj

--- Entrega3/generar_contraseñas.sql
+++ Entrega3/generar_contraseñas.sql
@@ -5,7 +5,7 @@ RETURNS void AS $$
 BEGIN
     IF NOT EXISTS(SELECT * FROM information_schema.COLUMNS WHERE COLUMN_NAME = 'contraseña' AND TABLE_NAME='usuarios_web') THEN
     ALTER TABLE usuarios_web ADD COLUMN contraseña varchar(100);
-    END IF; 
+    END IF;
 
     UPDATE usuarios_web
     SET contraseña = SUBSTRING(rut,1,4);
