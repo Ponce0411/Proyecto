0f6baa69f105ee51866a271f3cbae65b8e6f156f
martin alonso ponce carrasco <martin.poncec@uc.cl>
2021-07-05 05:02:28 -0400

    Avance

--- Entrega3/listado_compras.sql
+++ Entrega3/listado_compras.sql
@@ -1,22 +1,23 @@
 CREATE OR REPLACE FUNCTION
-insertar_usuarios(id INT)
+listado_compra(id INT)
 RETURNS void AS $$
 
 DECLARE
 tupla_compra RECORD ;
 
 BEGIN
-    FOR tupla_compra IN (SELECT * FROM public.dblink('dbname=grupo48e3 
-    port=5432
-    password=tuckathomas123
-    user=grupo48','SELECT despacho.id_compra, despacho.fecha FROM despacho') AS f(id_compra int, fecha date))
-
     IF NOT EXISTS(SELECT * FROM information_schema.COLUMNS WHERE COLUMN_NAME = 'fecha' AND TABLE_NAME='compras') THEN
     ALTER TABLE compras ADD COLUMN fecha date;
     END IF; 
 
+    FOR tupla_compra IN (SELECT * FROM public.dblink('dbname=grupo48e3 
+    port=5432
+    password=tuckathomas123
+    user=grupo48','SELECT despachos.id_compra, despachos.fecha FROM despachos') AS f(id_compra int, fecha date))
     UPDATE compras
-    SET fecha = tupla_compra.fecha
+    SET fecha = listado.fecha
+    FROM compras, (SELECT compras.id, tupla_compra.fecha FROM compras, tupla_compra WHERE tupla_compra.id_compra = compras.id) 
+    AS listado WHERE tupla_compra.id_compra = compras.id
 END
 
 $$ language plpgsql 
