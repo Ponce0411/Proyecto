9925549b99048d78e07a099a339d44b1d2f47422
martin alonso ponce carrasco <martin.poncec@uc.cl>
2021-07-04 23:10:03 -0400

    Avance

--- Entrega3/generar_contraseñas.sql
+++ Entrega3/generar_contraseñas.sql
@@ -2,13 +2,13 @@ CREATE OR REPLACE FUNCTION
 generar_contraseña()
 RETURNS void AS $$ 
 BEGIN
-IF NOT EXISTS(SELECT * FROM information_schema.COLUMNS WHERE COLUMN_NAME = 'contraseña' AND TABLE_NAME='usuarios_web') THEN
-ALTER TABLE usuarios_web ADD COLUMN contraseña varchar(4) NOT NULL default '';
-END IF; 
+    IF NOT EXISTS(SELECT * FROM information_schema.COLUMNS WHERE COLUMN_NAME = 'contraseña' AND TABLE_NAME='usuarios_web') THEN
+    ALTER TABLE usuarios_web ADD COLUMN contraseña varchar(4) NOT NULL default '';
+    END IF; 
 
-UPDATE usuarios_web
-SET contraseña = clave
-FROM (SELECT SUBSTRING(rut,1,4) as clave FROM usuarios_web) 
+    UPDATE usuarios_web
+    SET contraseña = clave
+    FROM (SELECT SUBSTRING(rut,1,4) as clave FROM usuarios_web) 
 END
 
 $$ language plpgsql
