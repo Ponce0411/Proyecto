aa6b60488b1bd9692e22235ce7c0e0d2a77227d7
martin alonso ponce carrasco <martin.poncec@uc.cl>
2021-07-04 23:41:51 -0400

    Avance

--- Entrega3/generar_contraseñas.sql
+++ Entrega3/generar_contraseñas.sql
@@ -2,9 +2,6 @@ CREATE OR REPLACE FUNCTION
 generar_contraseña()
 RETURNS void AS $$ 
 
-DECLARE
-clave RECORD ;
-
 BEGIN
     IF NOT EXISTS(SELECT * FROM information_schema.COLUMNS WHERE COLUMN_NAME = 'contraseña' AND TABLE_NAME='usuarios_web') THEN
     ALTER TABLE usuarios_web ADD COLUMN contraseña varchar(4);
