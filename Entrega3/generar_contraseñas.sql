207b7f94236771156631356eb8ed994d74817119
martin alonso ponce carrasco <martin.poncec@uc.cl>
2021-07-04 23:15:39 -0400

    Avance

--- Entrega3/generar_contraseñas.sql
+++ Entrega3/generar_contraseñas.sql
@@ -1,9 +1,10 @@
-CREATE PROCEDURE
+CREATE OR REPLACE FUNCTION
 generar_contraseña()
+RETURNS void AS $$ 
 
 BEGIN
     IF NOT EXISTS(SELECT * FROM information_schema.COLUMNS WHERE COLUMN_NAME = 'contraseña' AND TABLE_NAME='usuarios_web') THEN
-    ALTER TABLE usuarios_web ADD COLUMN contraseña varchar(4) NOT NULL default '';
+    ALTER TABLE usuarios_web ADD COLUMN contraseña varchar(4);
     END IF; 
 
     UPDATE usuarios_web
