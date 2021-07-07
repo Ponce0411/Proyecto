3a3b9720c3341eb83ea124c9c6a9ff47af688548
Lucas <69730343+Lucassalcedo@users.noreply.github.com>
2021-07-07 14:57:47 -0400

    faaf

--- Entrega3/generar_compras.sql
+++ Entrega3/generar_compras.sql
@@ -7,7 +7,7 @@ BEGIN
     
 
     IF pid IN (SELECT productos_en_tienda.producto_id FROM productos_en_tienda WHERE productos_en_tienda.tienda_id = tid) THEN
-    INSERT INTO compras_web VALUES(cid, uwid, did, tid,CONVERT(varchar,GETDATE());
+    INSERT INTO compras_web VALUES(cid, uwid, did, tid,CONVERT(varchar,GETDATE()));
     INSERT INTO productos_en_compra VALUES(cid, did, cant);
     END IF;
 
