70f0188edd1b9855bfe03e9203691c6e5618a825
Lucas <69730343+Lucassalcedo@users.noreply.github.com>
2021-07-07 15:10:36 -0400

    akksk

--- Entrega3/generar_compras.sql
+++ Entrega3/generar_compras.sql
@@ -14,7 +14,7 @@ BEGIN
     SELECT INTO fecha_actual CONVERT(varchar, funcion);
 
     IF pid IN (SELECT productos_en_tienda.producto_id FROM productos_en_tienda WHERE productos_en_tienda.tienda_id = tid) THEN
-    INSERT INTO compras_web VALUES(cid, uwid, did, tid,SUBSTRING(fecha_actual,1,10);
+    INSERT INTO compras VALUES(cid, uwid, did, tid,SUBSTRING(fecha_actual,1,10);
     INSERT INTO productos_en_compra VALUES(cid, did, cant);
     END IF;
 
