01d6b7b46ff0bd2e4d436a3f4e5fd8554a228c87
martin alonso ponce carrasco <martin.poncec@uc.cl>
2021-07-07 04:13:14 -0400

    S

--- Entrega3/generar_compras.sql
+++ Entrega3/generar_compras.sql
@@ -6,7 +6,7 @@ BEGIN
 
     IF pid IN (SELECT productos_en_tienda.producto_id FROM productos_en_tienda WHERE productos_en_tienda.tienda_id = tid) THEN
     INSERT INTO compras_web VALUES(cid, uwid, did, tid, '07-07-2021');
-    INSERT INTO productos_en_compra VALUES(cid, did, cant);
+    INSERT INTO productos_en_compra_web VALUES(cid, did, cant);
     END IF;
 
 END
