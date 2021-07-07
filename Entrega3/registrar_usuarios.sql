42bf90d938a244af90885db3e938205348096fd0
Lucas <69730343+Lucassalcedo@users.noreply.github.com>
2021-07-07 18:26:47 -0400

    hahah

--- Entrega3/registrar_usuarios.sql
+++ Entrega3/registrar_usuarios.sql
@@ -17,20 +17,21 @@ BEGIN
     MAX(direcciones.id) 
     FROM direcciones;
 
-    IF (verificador NOT IN (SELECT usuarios.rut FROM usuarios) AND NOT (direccion IN (SELECT direcciones.nombre FROM direcciones) 
-    AND comunas IN (SELECT direcciones.comuna FROM direcciones))) THEN   
+    IF (verificador NOT IN (SELECT usuarios.rut FROM usuarios) AND NOT (direccion IN (SELECT direcciones.nombre FROM direcciones)
+    AND comunas IN (SELECT direcciones.comuna FROM direcciones))) THEN
     INSERT INTO direcciones VALUES(maximo2+1,direccion,comunas);
-    INSERT INTO usuarios VALUES(maximo1+1,nombres,verificador,edad,sexo,direccion,SUBSTRING(verificador,1,4));
+    INSERT INTO usuarios VALUES(maximo1+1,nombres,verificador,edad,sexo,SUBSTRING(verificador,1,4));
     INSERT INTO direccion_usuarios VALUES(maximo2+1, maximo1+1);
     
     END IF;
 
-    IF (verificador NOT IN (SELECT usuarios.rut FROM usuarios) AND (direccion IN (SELECT direcciones.nombre FROM direcciones) 
-    AND comunas IN (SELECT direcciones.comuna FROM direcciones))) THEN 
+    IF (verificador NOT IN (SELECT usuarios.rut FROM usuarios) AND (direccion IN (SELECT direcciones.nombre FROM direcciones)
+    AND comunas IN (SELECT direcciones.comuna FROM direcciones))) THEN
     SELECT INTO id1
     id FROM direcciones WHERE direcciones.nombre = direccion AND direcciones.comuna = comunas; 
-    INSERT INTO usuarios VALUES(maximo1+1,nombres,verificador,edad,sexo,direccion,SUBSTRING(verificador,1,4));
+    INSERT INTO usuarios VALUES(maximo1+1,nombres,verificador,edad,sexo,SUBSTRING(verificador,1,4));
     INSERT INTO direccion_usuarios VALUES(id1, maximo1+1);
+
     END IF;
 
 END
