3c816edd8378b20689217741fddc6065453f0820
martin alonso ponce carrasco <martin.poncec@uc.cl>
2021-07-07 02:01:05 -0400

    S

--- Entrega3/generar_compras.sql
+++ Entrega3/generar_compras.sql
@@ -13,7 +13,7 @@ BEGIN
 
     IF pid IN (SELECT productos_en_tienda.producto_id FROM productos_en_tienda WHERE productos_en_tienda.tienda_id = tid) THEN
     INSERT INTO compras_web VALUES(maximo+1, uwid, did, tid);
-    INSERT INTO productos_en_tienda VALUES(maximo+1, did, cant);
+    INSERT INTO productos_en_compra VALUES(maximo+1, did, cant);
     END IF;
 
 END
