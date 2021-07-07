b9f806c4d99d54eb0e09d3435e2390c8fe288a23
martin alonso ponce carrasco <martin.poncec@uc.cl>
2021-07-07 02:37:16 -0400

    S

--- Entrega3/generar_compras.sql
+++ Entrega3/generar_compras.sql
@@ -1,19 +1,12 @@
 CREATE OR REPLACE FUNCTION
-generar_compra(pid int, uwid int, did int, tid int, cant int)
+generar_compra(cid int, pid int, uwid int, did int, tid int, cant int)
 RETURNS void AS $$
-
-DECLARE
-maximo INT ;
 
 BEGIN
 
-    SELECT INTO maximo
-    MAX(compras_web.id)
-    FROM compras_web;
-
     IF pid IN (SELECT productos_en_tienda.producto_id FROM productos_en_tienda WHERE productos_en_tienda.tienda_id = tid) THEN
-    INSERT INTO compras_web VALUES(maximo+1, uwid, did, tid);
-    INSERT INTO productos_en_compra VALUES(maximo+1, did, cant);
+    INSERT INTO compras_web VALUES(cid, uwid, did, tid);
+    INSERT INTO productos_en_compra VALUES(cid, did, cant);
     END IF;
 
 END
