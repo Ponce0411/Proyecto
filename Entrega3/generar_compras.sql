ddd5328276b92762e9a9cef522bf1066b5d7729c
martin alonso ponce carrasco <martin.poncec@uc.cl>
2021-07-07 01:49:18 -0400

    S

--- Entrega3/generar_compras.sql
+++ Entrega3/generar_compras.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE FUNCTION
-generar_compra(pid int, uwid int, did int, tid int)
+generar_compra(pid int, uwid int, did int, tid int, cant int)
 RETURNS void AS $$
 
 DECLARE
@@ -13,6 +13,7 @@ BEGIN
 
     IF pid IN (SELECT productos_en_tienda.producto_id FROM productos_en_tienda WHERE productos_en_tienda.tienda_id = tid) THEN
     INSERT INTO compras_web VALUES(maximo+1, uwid, did, tid);
+    INSERT INTO productos_en_tienda VALUES(maximo+1, did, cant);
     END IF;
 
 END
