9fda75532158dffddcec5d07038d630113d87301
martin alonso ponce carrasco <martin.poncec@uc.cl>
2021-07-07 00:05:08 -0400

    s

--- Entrega3/pico.sql
+++ Entrega3/pico.sql
@@ -6,14 +6,15 @@ DECLARE
 tupla RECORD ;
 
 BEGIN
-    FOR tupla IN (SELECT direcciones.id as id1, usuarios_web.id as id2 FROM direcciones, usuarios_web WHERE usuarios_web.direccion = direcciones.nombre)
+    FOR tupla IN (SELECT direcciones.id as id1, usuarios_web.id as id2 FROM direcciones, usuarios_web 
+    WHERE usuarios_web.direccion = direcciones.nombre)
 
 
     LOOP
 
-    IF tupla.id2 NOT IN (SELECT direccion_usuarios.usuario_id FROM direccion_usuarios) AND tupla.id1 NOT IN (SELECT direccion_usuarios.direccion_id FROM direccion_usuarios) THEN
+    IF tupla.id2 NOT IN (SELECT direccion_usuarios_web.usuario_id FROM direccion_usuarios_web) AND tupla.id1 NOT IN (SELECT direccion_usuarios_web.direccion_id FROM direccion_usuarios_web) THEN
 
-    INSERT INTO direccion_usuarios VALUES(tupla.id1, tupla.id2);
+    INSERT INTO direccion_usuarios_web VALUES(tupla.id1, tupla.id2);
     
     END IF;
 
