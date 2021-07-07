442f8ceee445f80b36f082f620f53465e164b181
Lucas <69730343+Lucassalcedo@users.noreply.github.com>
2021-07-07 14:56:08 -0400

    aggs

--- Entrega3/generar_compras.sql
+++ Entrega3/generar_compras.sql
@@ -4,9 +4,11 @@ RETURNS void AS $$
 
 BEGIN
 
+    
+
     IF pid IN (SELECT productos_en_tienda.producto_id FROM productos_en_tienda WHERE productos_en_tienda.tienda_id = tid) THEN
-    INSERT INTO compras_web VALUES(cid, uwid, did, tid, '07-07-2021');
-    INSERT INTO productos_en_compra_web VALUES(cid, did, cant);
+    INSERT INTO compras_web VALUES(cid, uwid, did, tid,CONVERT(varchar,GETDATE());
+    INSERT INTO productos_en_compra VALUES(cid, did, cant);
     END IF;
 
 END
