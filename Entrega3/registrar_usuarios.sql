f2c803b0d5542b086911c17c462ae451c7f080b6
Lucas <69730343+Lucassalcedo@users.noreply.github.com>
2021-07-07 12:39:37 -0400

    ajaj

--- Entrega3/registrar_usuarios.sql
+++ Entrega3/registrar_usuarios.sql
@@ -10,26 +10,26 @@ id1 INT ;
 BEGIN
 
     SELECT INTO maximo1
-    MAX(usuarios_web.id) 
-    FROM usuarios_web;
+    MAX(usuarios.id) 
+    FROM usuarios;
 
     SELECT INTO maximo2
     MAX(direcciones.id) 
     FROM direcciones;
 
-    IF (verificador NOT IN (SELECT usuarios_web.rut FROM usuarios_web) AND NOT (direccion IN (SELECT direcciones.nombre FROM direcciones) 
+    IF (verificador NOT IN (SELECT usuarios.rut FROM usuarios) AND NOT (direccion IN (SELECT direcciones.nombre FROM direcciones) 
     AND comunas IN (SELECT direcciones.comuna FROM direcciones))) THEN   
     INSERT INTO direcciones VALUES(maximo2+1,direccion,comunas);
-    INSERT INTO direccion_usuarios_web VALUES(maximo2+1, maximo1+1);
-    INSERT INTO usuarios_web VALUES(maximo1+1,nombres,verificador,edad,sexo,direccion,SUBSTRING(verificador,1,4));
+    INSERT INTO direccion_usuarios VALUES(maximo2+1, maximo1+1);
+    INSERT INTO usuarios VALUES(maximo1+1,nombres,verificador,edad,sexo,direccion,SUBSTRING(verificador,1,4));
     END IF;
 
-    IF (verificador NOT IN (SELECT usuarios_web.rut FROM usuarios_web) AND (direccion IN (SELECT direcciones.nombre FROM direcciones) 
+    IF (verificador NOT IN (SELECT usuarios.rut FROM usuarios) AND (direccion IN (SELECT direcciones.nombre FROM direcciones) 
     AND comunas IN (SELECT direcciones.comuna FROM direcciones))) THEN 
     SELECT INTO id1
     id FROM direcciones WHERE direcciones.nombre = direccion AND direcciones.comuna = comunas; 
-    INSERT INTO usuarios_web VALUES(maximo1+1,nombres,verificador,edad,sexo,direccion,SUBSTRING(verificador,1,4));
-    INSERT INTO direccion_usuarios_web VALUES(id1, maximo1+1);
+    INSERT INTO usuarios VALUES(maximo1+1,nombres,verificador,edad,sexo,direccion,SUBSTRING(verificador,1,4));
+    INSERT INTO direccion_usuarios VALUES(id1, maximo1+1);
     END IF;
 
 END
