24d67b70eb773b7f68312497034c81d2036ba896
Lucas <69730343+Lucassalcedo@users.noreply.github.com>
2021-07-07 19:02:37 -0400

    gaga

--- Entrega3/generar_compras.sql
+++ Entrega3/generar_compras.sql
@@ -5,13 +5,13 @@ RETURNS void AS $$
 BEGIN
 
     IF pid IN (SELECT productos_en_tienda.producto_id FROM productos_en_tienda WHERE productos_en_tienda.tienda_id = tid) AND cid IN (SELECT id FROM compras) THEN
-    INSERT INTO productos_en_compra VALUES(cid, did, cant);
+    INSERT INTO productos_en_compra VALUES(cid, pid, cant);
     END IF;
     
 
     IF pid IN (SELECT productos_en_tienda.producto_id FROM productos_en_tienda WHERE productos_en_tienda.tienda_id = tid) AND cid NOT IN (SELECT id FROM compras) THEN
     INSERT INTO compras VALUES(cid, uwid, did, tid, fecha);
-    INSERT INTO productos_en_compra VALUES(cid, did, cant);
+    INSERT INTO productos_en_compra VALUES(cid, pid, cant);
     END IF;
 
 END
