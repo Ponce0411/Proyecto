789e2bbf3aecaac3dfaa817ffb3c197c719a830d
Lucas <69730343+Lucassalcedo@users.noreply.github.com>
2021-07-07 15:18:23 -0400

    gaagag

--- Entrega3/generar_compras.sql
+++ Entrega3/generar_compras.sql
@@ -5,7 +5,7 @@ RETURNS void AS $$
 BEGIN
 
     IF pid IN (SELECT productos_en_tienda.producto_id FROM productos_en_tienda WHERE productos_en_tienda.tienda_id = tid) THEN
-    INSERT INTO compras VALUES(cid, uwid, did, tid,SUBSTRING( CONVERT(varchar, GETDATE()),1,10));
+    INSERT INTO compras VALUES(cid, uwid, did, tid,SUBSTRING(CAST(GETDATE() AS varchar),1,10));
     INSERT INTO productos_en_compra VALUES(cid, did, cant);
     END IF;
 
