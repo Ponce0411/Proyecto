c6ec58aab5e57740c7867b960488caa3d74721d5
Lucas <69730343+Lucassalcedo@users.noreply.github.com>
2021-07-07 15:13:45 -0400

    ahaah

--- Entrega3/generar_compras.sql
+++ Entrega3/generar_compras.sql
@@ -4,14 +4,11 @@ RETURNS void AS $$
 
 DECLARE
 
-funcion date;
 fecha_actual varchar;
 
 BEGIN
 
-    SELECT INTO funcion GETDATE();
-
-    SELECT INTO fecha_actual CONVERT(varchar, funcion);
+    SELECT INTO fecha_actual CONVERT(varchar, GETDATE());
 
     IF pid IN (SELECT productos_en_tienda.producto_id FROM productos_en_tienda WHERE productos_en_tienda.tienda_id = tid) THEN
     INSERT INTO compras VALUES(cid, uwid, did, tid,SUBSTRING(fecha_actual,1,10));
