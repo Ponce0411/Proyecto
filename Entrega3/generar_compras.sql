7289e544c0c4fafd15803d600f9097cd5f7f444f
Lucas <69730343+Lucassalcedo@users.noreply.github.com>
2021-07-07 15:08:27 -0400

    fafaf

--- Entrega3/generar_compras.sql
+++ Entrega3/generar_compras.sql
@@ -2,12 +2,19 @@ CREATE OR REPLACE FUNCTION
 generar_compra(cid int, pid int, uwid int, did int, tid int, cant int)
 RETURNS void AS $$
 
+DECLARE
+
+funcion date;
+fecha_actual varchar;
+
 BEGIN
 
-    
+    SELECT INTO funcion GETDATE();
+
+    SELECT INTO fecha_actual CONVERT(varchar, funcion);
 
     IF pid IN (SELECT productos_en_tienda.producto_id FROM productos_en_tienda WHERE productos_en_tienda.tienda_id = tid) THEN
-    INSERT INTO compras_web VALUES(cid, uwid, did, tid,CONVERT(varchar,GETDATE()));
+    INSERT INTO compras_web VALUES(cid, uwid, did, tid,SUBSTRING(fecha_actual,1,10);
     INSERT INTO productos_en_compra VALUES(cid, did, cant);
     END IF;
 
