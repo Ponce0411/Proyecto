2c88535e4f310af02687fa307b23c13599c6a4a2
martin alonso ponce carrasco <martin.poncec@uc.cl>
2021-07-07 03:09:39 -0400

    Avance

--- Entrega3/generar_compras.sql
+++ Entrega3/generar_compras.sql
@@ -2,10 +2,16 @@ CREATE OR REPLACE FUNCTION
 generar_compra(cid int, pid int, uwid int, did int, tid int, cant int)
 RETURNS void AS $$
 
+DECLARE
+
+hora DATE ;
+
 BEGIN
 
+    SELECT INTO hora GETDATE;
+
     IF pid IN (SELECT productos_en_tienda.producto_id FROM productos_en_tienda WHERE productos_en_tienda.tienda_id = tid) THEN
-    INSERT INTO compras_web VALUES(cid, uwid, did, tid);
+    INSERT INTO compras_web VALUES(cid, uwid, did, tid, hora);
     INSERT INTO productos_en_compra VALUES(cid, did, cant);
     END IF;
 
